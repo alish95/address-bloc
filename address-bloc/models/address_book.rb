8fa5c2cbb6c3f290f637d11c43832f539b1fdfe7
Alice <alicewc95@gmail.com>
2017-11-15 16:08:42 -0800

    checkpoint-6

--- address-bloc/models/address_book.rb
+++ address-bloc/models/address_book.rb
@@ -1,4 +1,6 @@
 require_relative 'entry'
+require "csv"
+
 class AddressBook
   attr_reader :entries
 
@@ -8,24 +10,23 @@ class AddressBook
 
   def add_entry(name, phone_number, email)
     index = 0
-    @entries.each do |entry|
+    entries.each do |entry|
       if name < entry.name
         break
       end
       index += 1
     end
 
-    @entries.insert(index, Entry.new(name, phone_number, email))
+    entries.insert(index, Entry.new(name, phone_number, email))
   end
 
-  def remove_entry(name, phone_number, email)
-    delete_entry = nil
-    @entries.each do |entry|
-      if name == entry.name && phone_number == entry.phone_number && email == entry.email
-        delete_entry = entry
-      end
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
-    @entries.delete(delete_entry)
   end
 
 end
