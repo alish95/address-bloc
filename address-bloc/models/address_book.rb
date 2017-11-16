e2f59958e3908e4d5706ddb123d394bde9635302
Alice <alicewc95@gmail.com>
2017-11-15 16:45:13 -0800

    checkpoint-7

--- address-bloc/models/address_book.rb
+++ address-bloc/models/address_book.rb
@@ -9,24 +9,57 @@ class AddressBook
   end
 
   def add_entry(name, phone_number, email)
-    index = 0
-    entries.each do |entry|
-      if name < entry.name
-        break
+      index = 0
+      entries.each do |entry|
+        if name < entry.name
+          break
+        end
+        index+= 1
       end
-      index += 1
+      entries.insert(index, Entry.new(name, phone_number, email))
     end
 
-    entries.insert(index, Entry.new(name, phone_number, email))
+
+  def remove_entry(name, phone_number, email)
+      entries.delete_if{|entry| entry.name == name }
   end
 
   def import_from_csv(file_name)
     csv_text = File.read(file_name)
     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
     csv.each do |row|
-      row_hash = row.to_hash
-      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+       row_hash = row.to_hash
+       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+     end
+  end
+
+  def binary_search(name)
+    lower = 0
+    upper = entries.length - 1
+
+     while lower <= upper
+       mid = (lower + upper) / 2
+       mid_name = entries[mid].name
+
+       if name == mid_name
+         return entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
+
+     return nil
+  end
+
+  def iterative_search(name)
+    for i in 0...entries.length
+      if entries[i].name == name
+        return entries[i]
+      end
     end
+    return nil
   end
 
 end
